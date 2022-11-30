dc3aba31baba9574f4b2a2851a867bd334fe4b31
Antonina Buchak <antoninabucak@gmail.com>
2022-11-30 19:21:02 +0200

    Updt

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -3,14 +3,15 @@ import Notiflix from 'notiflix';
 const BASE_URL = 'https://restcountries.com/v2/name/';
 const filters = 'name,capital,population,flags,languages';
 
-
-
 export default function fetchCountries(name) {
-  return fetch(`${BASE_URL}${name}?fields=${filters}`).then(response => {
-    if (!response.ok) {
-  
-      throw new Error(Notiflix.Notify.failure('Oops, there is no country with that name'));
-    }
-    return response.json();
-  }).catch(err=>console.error(err));
+  return fetch(`${BASE_URL}${name}?fields=${filters}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(
+          Notiflix.Notify.failure('Oops, there is no country with that name')
+        );
+      }
+      return response.json();
+    })
+    .catch(err => console.error(err));
 }
