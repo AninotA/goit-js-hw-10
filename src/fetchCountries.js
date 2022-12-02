5b029e205ce5c89fffd1b1eaaee98edd19d66543
Antonina Buchak <antoninabucak@gmail.com>
2022-12-02 10:27:10 +0200

    finish

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,30 +1,19 @@
-// export function fetchCountries(name) {
-//   const BASE_URL = 'https://restcountries.com/v3.1/name/';
-//   const filters = 'name,capital,population,flags,languages';
-
-//   return fetch(`${BASE_URL}/${name}?fields=${filters}`).then(response => {
-//     if (!response.ok) {
-//       throw new Error(response.statusText);
-//     }
-//     return response.json();
-//   });
-//   // .catch(err => console.error(err));
-// }
-
-const BASE_URL = 'https://restcountries.com/v3.1/name/';
-const filters = 'name,capital,population,flags,languages';
-
-function notFound() {
-  Notiflix.Notify.failure("Oops, there is no country with that name")
-}
-
-export default function fetchCountries(name) {
-  return fetch(`${BASE_URL}/${name}?fields=${filters}`).then(response => {
-        if (!response.ok) {
-          notFound()
-          throw Error(statusText);
-        }
-        return response.json();
+export const fetchCountries = name => {
+  return fetch(
+      `https://restcountries.com/v3.1/name/${name}?fields=,name,capital,population,flags,languages`
+  )
+      .then(response => {
+          if (!response.ok) {
+              if (response.status === 404) {
+                  return [];
+              }
+              throw new Error(response.status);
+          }
+          return response.json();
+      })
+      .catch(error => {
+          console.error(error);
       });
-   
-}
+};
+
+
--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import './css/styles.css';
-import debounce from 'lodash.debounce';
 import { fetchCountries } from './fetchCountries';
+import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
-import 'notiflix/dist/notiflix-3.2.5.min.css';
 
+const input = document.querySelector('#search-box');
+const list = document.querySelector('.country-list');
+const info = document.querySelector('.country-info');
 const DEBOUNCE_DELAY = 300;
-const search = document.querySelector('#search-box');
-const list = document.querySelector('country-list');
-const info = document.querySelector('country-info');
 
-search.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
+
+input.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
 
 function onSearch(e) {
   let nameCountry = e.target.value.trim();
@@ -22,10 +22,12 @@ function onSearch(e) {
   fetchCountries(nameCountry)
     .then(data => {
       if (data.length > 10) {
-        list.innerHTML = '';
-        info.innerHTML = '';
+        // list.innerHTML = '';
+        // info.innerHTML = '';
         manyMatchesFound(data);
         return;
+      } else if (data.length === 0) {
+        Notiflix.Notify.failure('Oops, there is no country with that name');
       } else if (data.length <= 10 && data.length >= 2) {
         createListCountry(data);
       } else if (data.length === 1) {
@@ -41,13 +43,13 @@ function manyMatchesFound() {
   );
 }
 
-function createListCountry(country) {
-  return country
-    .map(
-      item =>
-        `<li class="country__item"><img src="${item.flags.svg}" alt="${item.name.official}" width="30" hight="20">${item.name.official}</li>`
-    )
+function createListCountry(countries) {
+  const markup = countries
+    .map(country => {
+      return `<li class="country__item"><img src="${country.flags.svg}" alt="${country.name.official}" width="40">${country.name.official}</li>`;
+    })
     .join('');
+  list.innerHTML = markup;
 }
 
 function createMarkupCountry(arr) {
@@ -55,7 +57,7 @@ function createMarkupCountry(arr) {
     .map(({ name, flags, capital, population, languages }) => {
       return `<div class="title"><img class="flag" src="${
         flags.svg
-      }" alt="${name}" width="70" /><p class="country">${
+      }" alt="${name}" width="100" /><p class="country">${
         name.official
       }</p></div>
             <p class="info"><span class="info">Capital: </span>${capital}</p>
@@ -65,7 +67,7 @@ function createMarkupCountry(arr) {
             )}</p>`;
     })
     .join('');
-  info.innerHTML = markup;
+  list.innerHTML = markup;
 }
 
 function cleanHtml() {
